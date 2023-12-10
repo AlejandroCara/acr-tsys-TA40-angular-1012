7086d4661c4851d82f84f520e22a7e9828aa0dd9
Alejandro <alejandro.carayol@gmail.com>
2023-12-10 17:38:27 +0100

    Background 100

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,6 +22,8 @@ export class AppComponent implements OnInit{
             .body.style.backgroundImage = "url(../assets/wallpapersden.com_rick-and-morty-season-6_3840x2160.jpg)";
             this.elementRef.nativeElement.ownerDocument
             .body.style.backgroundSize = "cover";
+            this.elementRef.nativeElement.ownerDocument
+            .body.style.height = "100%";
     }
     
   ngOnInit() { 
